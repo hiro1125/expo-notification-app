984de370d69e3683b971caf1b6a60112c687838c
hiro1125 <hiro112538@gmail.com>
2024-06-20 22:32:38 +0900

    feat: デバイスプッシュトークンの取得

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,9 @@ import { useEffect } from 'react';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -16,7 +19,45 @@ Notifications.setNotificationHandler({
   }),
 });
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      console.log('プッシュ通知のトークン取得に失敗しました！');
+      return;
+    }
+
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      })
+    ).data;
+    console.log('プッシュ通知トークン:', token);
+  } else {
+    console.log('プッシュ通知は物理デバイスでのみ利用可能です');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
+
+// アセットの読み込みが完了する前にスプラッシュスクリーンが自動的に隠れないようにする
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -24,6 +65,15 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  // プッシュ通知の設定
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await registerForPushNotificationsAsync();
+      console.log('取得したトークン:', token);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (loaded) {
